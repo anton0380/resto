04a9d2879456a9578f1b19399619116494948937
anton0380 <aa@unelma.ru>
2021-07-22 11:55:43 +0300

    working with spinner

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,20 @@
 const initialState = {
-    menu: []
+    menu: [],
+    loading: true
 }
-
+// all need request, success and error
 const reducer = (state = initialState, action) => {
+    console.log(state);
     switch (action.type) {
         case 'MENU_LOADED':
             return {
-                menu: action.payload
+                menu: action.payload,
+                loading: false
+            };
+        case 'MENU_REQUESTED':
+            return {
+                menu: state.menu,
+                loading: true
             };
         default:
             return state;
