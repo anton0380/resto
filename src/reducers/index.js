7d3874ee4d389cd886f845f1b7463ca70623a991
anton0380 <aa@unelma.ru>
2021-07-22 13:02:08 +0300

    working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 const initialState = {
     menu: [],
-    loading: true
+    loading: true,
+    items: []
 }
 // all need request, success and error
 const reducer = (state = initialState, action) => {
@@ -8,14 +9,42 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'MENU_LOADED':
             return {
+                ...state,
                 menu: action.payload,
                 loading: false
             };
         case 'MENU_REQUESTED':
             return {
+                ...state,
                 menu: state.menu,
                 loading: true
             };
+        case 'ITEM_ADD_TO_CART':
+            const id = action.payload;
+            const item = state.menu.find(item => item.id === id);
+            const newItem = {
+                title: item.title,
+                price: item.price,
+                url: item.url,
+                id: item.id
+            };
+            return {
+                ...state,
+                items: [
+                    ...state.items,
+                    newItem
+                ]
+            };
+        case 'ITEM_REMOVE_FROM_CART':
+            const idx = action.payload;
+            const itemIndex = state.items.findIndex(item => item.id === idx);
+            return {
+                ...state,
+                items: [
+                    ...state.items.slice(0, itemIndex),
+                    ...state.items.slice(itemIndex + 1)
+                ]
+            }
         default:
             return state;
     }
